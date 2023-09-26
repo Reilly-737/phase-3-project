8366fa3a5550e12fd9760956ba9c554a5a5eeb26
Madison <mlinkenmeyer@gmail.com>
2023-09-25 18:51:02 -0700

    some logic for the first option

--- lib/cli.py
+++ lib/cli.py
@@ -2,27 +2,65 @@
 
 from helpers import (
     exit_program,
-    helper_1
+    print_slowly,
+    head_outside,
+    game_over
 )
 
 
-def main():
+# def main():
+#     while True:
+#         menu()
+#         choice = input("> ")
+#         if choice == "0":
+#             exit_program()
+#         elif choice == "1":
+#             helper_1()
+#         else:
+#             print("Invalid choice")
+
+def first_option():
     while True:
-        menu()
+        start_adventure()
         choice = input("> ")
         if choice == "0":
-            exit_program()
+            game_over()
         elif choice == "1":
-            helper_1()
+            head_outside()
+        elif choice == "2":
+            exit_program()
         else:
             print("Invalid choice")
 
 
-def menu():
-    print("Please select an option:")
-    print("0. Exit the program")
-    print("1. Some useful function")
+# def menu():
+#     print("Please select an option:")
+#     print("0. Create a user")
+#     print("1. Log in with existing username and password")
+
+
+def start_adventure():
+    print_slowly("""
+          You find yourself amidst a vibrant party with your friends, 
+          the music pulsating through the air as laughter fills the room. 
+          You're faced with a choice: 
+          
+          """)
+    print_slowly("""
+                0. Join your friends for more shots and dive headfirst into 
+                the night's revelry.
+          
+          """)
+    print_slowly("""
+                1. Opt for a more mellow approach, sipping on lemonade 
+                as you enjoy the company of your friends.
+          
+          """)
+    print_slowly("""
+                2. Exit game.
+          
+          """)
 
 
 if __name__ == "__main__":
-    main()
+    first_option()
--- lib/helpers.py
+++ lib/helpers.py
@@ -1,9 +1,29 @@
 # lib/helpers.py
 
+import time
+
+
 def helper_1():
     print("Performing useful function#1.")
+
+
+def game_over():
+    print("Game over. Goodbye!")
+    exit()
+
+
+def head_outside():
+    print("You're outside!")
 
 
 def exit_program():
     print("Goodbye!")
     exit()
+
+
+def print_slowly(output):
+    for char in output:
+        print(char, end='', flush=True)
+        time.sleep(0.008)
+        # time.sleep(0)
+    print()
