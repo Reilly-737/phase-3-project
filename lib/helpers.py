8cfb4f8abcf3b843ab5f764ad92e2add5e5ad24c
+ <+>
2023-09-27 11:07:43 -0600

    updated code

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,65 +1,63 @@
 # lib/helpers.py
-import sqlite3 
-DATABASE_NAME = "game.db"
 
-def create_database():
-    try:
-        conn = sqlite3.connect(DATABASE_NAME)
-        cursor = conn.cursor()
-        
-        conn.commit()
-        conn.close()
-    except sqlite3.Error as e:
-        print(f"Database error: {e}")
-        
-def start_new_game(player_name):
-    try:
-        conn = sqlite3.connect(DATABASE_NAME)
-        cursor = conn.cursor()
-        
-        cursor.execute("SELECT * FROM scenes WHERE scene_id = 0")
-        introduction = cursor.fetchone() 
-        
-        cursor.execute("UPDATE players SET scene_id = ? WHERE player_name = ?",(introduction[0], player_name))
-        
-        conn.commit()
-        conn.close()
-        print(f"New game started for {player_name}.")
-    except sqlite3.Error as e: 
-        print(f"Database error: {e}")
-        
-def continue_game(player_name):
-    try:
-        conn = sqlite3.connect(DATABASE_NAME)
-        cursor = conn.cursor()
-        
-        conn.commit()
-        conn.close()
-        print(f"Continuing game for {player_name}.")
-    except sqlite3.Error as e:
-        print(f"Database error: {e}")
- 
-def delete_game_data(player_name):
-    try: 
-        conn = sqlite3.connect(DATABASE_NAME)
-        cursor = conn.cursor()
-        
-        cursor.execute("DELETE FROM player_profiles WHERE playername= ?", (player_name))
-        
-        conn.commit()
-        conn.close()
-        
-        print(f"Game data for {player_name} deleted.")
-    except sqlite3.Error as e:
-        print(f"Database error: {e}")
-        
-def exit_game():
-    print("Exiting the game")
-    
-def exit_program():
-    print("Goodbye!")
-    exit()
+from models.model_1 import Player
 
+import time
+
+import ipdb
+
+
+def list_players():
+    players = Player.get_all()
+    for player in players:
+        print(player.player_name)
+
+
+def create_player():
+    player_name = input("Enter your player_name: ")
+    scene_id = input("Enter the current scene id: ")
+    try:
+        player = Player.create(player_name, scene_id)
+        print(f"Success: {player.player_name}")
+    except Exception as exc:
+        print("Error creating player_name: ", exc)
+
+
+def update_player():
+    id_ = input("Enter the player's id: ")
+    if player := Player.find_by_id(id_):
+        try:
+            name = input("Enter the players new name: ")
+            player.player_name = name
+            scene = input("Enter the player's current scene: ")
+            player.scene_id = scene
+
+            player.update()
+            print(f'Success: {player}')
+        except Exception as exc:
+            print("Error updating player: ", exc)
+    else:
+        print(f'Player {id_} not found')
+
+
+def delete_player():
+    id_ = input("Enter the player's id: ")
+    if player := Player.find_by_id(id_):
+        player.delete()
+        print(f'Player {id_} deleted')
+    else:
+        print(f'Player {id_} not found')
+
+
+ipdb.set_trace()
+
+
+def print_slowly(output):
+    for char in output:
+        print(char, end='', flush=True)
+        time.sleep(0.008)
+        # time.sleep(0)
+    print()
 
 
 
--- lib/models/__init__.py
+++ lib/models/__init__.py
@@ -1,20 +1,6 @@
  #lib/models/__init__.py
 import sqlite3
 
-def initialize_database():
-    try:
-        conn = sqlite3.connect('game_database.db')
-        cursor = conn.cursor()
 
-        cursor.execute('''
-            CREATE TABLE IF NOT EXISTS user_profiles (
-                id INTEGER PRIMARY KEY,
-                username TEXT UNIQUE,
-                progress TEXT
-            )
-        ''')
-
-        conn.commit()
-        conn.close()
-    except sqlite3.Error as e:
-        print(f"Database error: {e}")
+CONN = sqlite3.connect('game.db')
+CURSOR = CONN.cursor()
