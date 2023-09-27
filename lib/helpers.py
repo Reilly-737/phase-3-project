852cc4207c085f631bee0bb2cc4453dc7d79bb72
+ <+>
2023-09-27 10:44:29 -0600

    update before rebase

--- lib/helpers.py
+++ lib/helpers.py
@@ -1,40 +1,61 @@
 # lib/helpers.py
 import sqlite3 
-#create function to create users login users and delete users
-def create_user(username):
-    try: 
-        conn = sqlite3.connect('game_database.db')
+DATABASE_NAME = "game.db"
+
+def create_database():
+    try:
+        conn = sqlite3.connect(DATABASE_NAME)
         cursor = conn.cursor()
-        cursor.execute('INSERT INTO user_profiles (username, progress) VALUES (?,?) ', (username, ''))
+        
         conn.commit()
         conn.close()
-        return True
-    except sqlite3.IntegrityError:
-        print("Username already exists")
-        return False 
-    
-def login_user(username):
-    try:
-        conn = sqlite3.connect('game_database.db')
-        cursor = conn.cursor()
-        cursor.execute('SELECT * FROM user_profiles WHERE username = ?', (username,))
-        user = cursor.fetchone()
-        conn.close()
-        return user 
     except sqlite3.Error as e:
         print(f"Database error: {e}")
         
-
-def delete_user_progress(username):
+def start_new_game(player_name):
     try:
-        conn = sqlite3.connect('game_database.db')
+        conn = sqlite3.connect(DATABASE_NAME)
         cursor = conn.cursor()
-        cursor.execute('UPDATE user_profiles SET progress = "" WHERE username = ?', (username))
+        
+        cursor.execute("SELECT * FROM scenes WHERE scene_id = 0")
+        introduction = cursor.fetchone() 
+        
+        cursor.execute("UPDATE players SET scene_id = ? WHERE player_name = ?",(introduction[0], player_name))
+        
         conn.commit()
         conn.close()
+        print(f"New game started for {player_name}.")
     except sqlite3.Error as e: 
         print(f"Database error: {e}")
         
+def continue_game(player_name):
+    try:
+        conn = sqlite3.connect(DATABASE_NAME)
+        cursor = conn.cursor()
+        
+        conn.commit()
+        conn.close()
+        print(f"Continuing game for {player_name}.")
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+ 
+def delete_game_data(player_name):
+    try: 
+        conn = sqlite3.connect(DATABASE_NAME)
+        cursor = conn.cursor()
+        
+        cursor.execute("DELETE FROM player_profiles WHERE playername= ?", (player_name))
+        
+        conn.commit()
+        conn.close()
+        
+        print(f"Game data for {player_name} deleted.")
+    except sqlite3.Error as e:
+        print(f"Database error: {e}")
+        
+def exit_game():
+    print("Exiting the game")
+    
 def exit_program():
     print("Goodbye!")
     exit()
